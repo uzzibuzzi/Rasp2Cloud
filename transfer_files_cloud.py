11803d6e3e18afe441f670bd4ddf3c4335aff4f4
uzzibuzzi <achim_vollmer@gmx.net>
2022-03-02 17:24:02 +0100

    change in files list not workin

--- transfer_files_cloud.py
+++ transfer_files_cloud.py
@@ -6,6 +6,22 @@ Created on Wed Feb 23 12:05:21 2022
 """
 
 import os
+import csv
+import pandas as pd
+
+
+def writeLogfile(sendedFiles):
+    folder="C:\\Users\\vollmera\\Documents\\SVN_home\\trunk\\SW_app\\github\\Rasp2Cloud\\log_data\\"
+    transferlogfile=folder+"alreadySended.txt"
+    df = pd.DataFrame (sendedFiles, columns = ['sended'])
+    try:
+        mdf=pd.read_csv(transferlogfile)     
+        mdf=pd.concat([mdf, df])
+        mdf.to_csv(transferlogfile)
+    except:
+        df.to_csv(transferlogfile)
+        
+
 
 def makeFileList(path):
     dirs=os.listdir(path)
@@ -13,7 +29,15 @@ def makeFileList(path):
     for names in dirs:
         if names.endswith(".csv"):
             messfiles.append(names)
-    return messfiles
+    try: 
+        transferlogfile=path+"alreadySended.txt"
+        mdf=pd.read_csv(transferlogfile)
+        alreadySended=list(mdf["sended"])[:4]
+    except:
+        alreadySended=[]
+        set(messfiles)-set(alreadySended)
+    return set(messfiles)-alreadySended    
+        
 
 
 
@@ -21,12 +45,16 @@ def send2cloud_win():
     folder="C:\\KBApps\\AzCopy\\"
     folder="C:\\Users\\vollmera\\Documents\\SVN_home\\trunk\\SW_app\\github\\Rasp2Cloud\\log_data\\"
     messfiles=makeFileList(folder)                 
+    
+    messfiles=messfiles[:5]
+    
+    
     for everyfile in messfiles:
         filename=everyfile
         print("sending .... ",everyfile)        
         #filename="2022_February_23_104650.csv"
         os.system('C:\\KBApps\\AzCopy\\azcopy copy "'+folder+filename+'" "https://testbenchpi4.blob.core.windows.net/achimpi4?sv=2020-08-04&st=2022-01-19T09%3A09%3A47Z&se=2023-09-20T08%3A09%3A00Z&sr=c&sp=racwdxlt&sig=o5eJidUhVuRDuIbUI%2BnaaACwAkEH4rFma4gBCsyjX7k%3D"')
-
+    writeLogfile(messfiles)
     #    print('C:\\KBApps\\AzCopy\\azcopy copy '+folder+filename+'" "https://testbenchpi4.blob.core.windows.net/achimpi4?sv=2020-08-04&st=2022-01-19T09%3A09%3A47Z&se=2023-09-20T08%3A09%3A00Z&sr=c&sp=racwdxlt&sig=o5eJidUhVuRDuIbUI%2BnaaACwAkEH4rFma4gBCsyjX7k%3D"')
     
     
@@ -39,11 +67,10 @@ def send2cloud_rasp():
     folder="/home/pi/github/log_data/"
     messfiles=makeFileList(folder)                 
     print(messfiles)
-    
-    #sendString='sudo  blobxfer upload --storage-account testbenchpi4 --sas "?sv=2020-08-04&ss=bfqt&srt=co&sp=rwdlacupitfx&se=2022-01-30T17:03:10Z&st=2022-01-19T09:03:10Z&spr=https&sig=ykOpTDsq5TjWQ22ntcqrvgkhGg%2F2%2FUeb3qUbIen6MAg%3D" --remote-path achimpi4 --local-path /home/pi/Daq/files/'
-    
     for everyfile  in messfiles:
         os.system(sendString+str(folder)+str(everyfile))
+    writeLogfile(messfiles)
+    
     
 
 def send2cloud():    
