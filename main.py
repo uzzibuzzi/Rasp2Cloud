70a512a26648e69fb693954416f992e32d6bfc57
uzzibuzzi <achim_vollmer@gmx.net>
2022-03-02 11:48:27 +0100

    no logging

--- main.py
+++ main.py
@@ -13,30 +13,29 @@ from transfer_files_cloud import send2cloud
 from DRC import main
 
 def thread_function(name):
-    logging.info("Thread %s: starting", name)
+    #logging.info("Thread %s: starting", name)
     send2cloud()
     time.sleep(10)
-    logging.info("Thread %s: finishing", name)
+    #logging.info("Thread %s: finishing", name)
 
 
 
 
 if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO,
-                        datefmt="%H:%M:%S")
+#    logging.basicConfig(format=format, level=logging.INFO,datefmt="%H:%M:%S")
 
-    logging.info("Main    : before creating thread")
+ #   logging.info("Main    : before creating thread")
     x = threading.Thread(target=thread_function, args=(1,))
-    logging.info("Main    : before running thread")
+  #  logging.info("Main    : before running thread")
     x.start()
-    logging.info("Main    : wait for the thread to finish")
+   # logging.info("Main    : wait for the thread to finish")
     # x.join()
     while mainRecord():
         main()
         pass
         
-    logging.info("Main    : all done")
-    
+    #logging.info("Main    : all done")
+    print("dinbe")
     
  
