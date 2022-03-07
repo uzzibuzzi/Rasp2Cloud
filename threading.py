7b579b3b50900219a46e9956555bb205496e4cde
uzzibuzzi <achim_vollmer@gmx.net>
2022-03-07 16:21:47 +0100

    removed loogin in threading

--- threading.py
+++ threading.py
@@ -4,7 +4,7 @@ Created on Wed Feb 23 09:31:47 2022
 
 @author: vollmera
 """
-import logging
+#import logging
 import threading
 import time
 from fakeDaqRec import mainRecord
@@ -13,29 +13,20 @@ from transfer_files_cloud import send2cloud
 
 
 def thread_function(name):
-    logging.info("Thread %s: starting", name)
+
     send2cloud()
     time.sleep(10)
-    logging.info("Thread %s: finishing", name)
 
 
 
 
 if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO,
-                        datefmt="%H:%M:%S")
-
-    logging.info("Main    : before creating thread")
     x = threading.Thread(target=thread_function, args=(1,))
-    logging.info("Main    : before running thread")
+
     x.start()
-    logging.info("Main    : wait for the thread to finish")
     # x.join()
     while mainRecord():
         pass
-        
-    logging.info("Main    : all done")
-    
     
  
