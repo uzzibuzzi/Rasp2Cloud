61ff557aa61d607b8e7e1e07e0ba24b97090feaa
altgynr <101431677+altgynr@users.noreply.github.com>
2022-07-26 11:13:30 +0200

    update cronos2cloud with delete function cronos

--- Rasp2Cloud/cronos2cloud.py
+++ Rasp2Cloud/cronos2cloud.py
@@ -1,3 +1,4 @@
+### IMPORTS ###
 import ftplib
 import os, sys
 import subprocess
@@ -5,25 +6,28 @@ import shutil
 from azure.storage.blob import BlockBlobService
 import time
 
-### CONFIGURATION ###
+### CONFIGURATION CRONOS & AZURE ACCOUNT ###
 cronos_ip = '192.168.100.231'
 user = 'imc'
 pw = 'imc'
 azure_storage_name = "testbenchpi4"
+# IMPORTANT: sas_key needs to be typed manually into the "os_command" line in the function "send2cloud"
 azure_sas_key = "?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2023-08-09T17:30:05Z&st=2022-02-10T10:30:05Z&spr=https&sig=%2BkcIcyTpUK0tHDiSBW4gC%2FvZP%2BrvSa5YS2neC7X%2BnfM%3D"
 azure_path = 'achimpi4/Cronos2Cloud'
 azure_blob_container = 'achimpi4'
 # directory in Cronos to read from
 cronos_dir = 'pcmcia' 
 # directory in Raspberry to save the cronos files locally
-raspberry_dir = '/home/pi/Cronos2Cloud' 
+raspberry_dir = '/home/pi/Cronos2Cloud'
 
-def cronos_connect():
+
+def cronos_connect(message):
     """ establish the FTP connection to Cronos """
     try:
         ftp = ftplib.FTP(cronos_ip, user, pw)
         ftp.encoding = 'utf-8'
-        print ("Connection to Cronos established!")
+        if message == True:
+            print ("Connection to Cronos established!")
         return ftp
     except ftplib.error_perm as e:
         print("Connection to Cronos could not be established! Check IP, user and password.")
@@ -41,12 +45,12 @@ def send2cloud(read_path, azure_final_path):
     os.system(os_command)
 
 def get_cloud_content():
-    """ get content of blob storage """
+    """ get content in blob storage """
     block_blob_service = BlockBlobService(account_name=azure_storage_name, sas_token=azure_sas_key)
     return block_blob_service
 
 def check_cloud_content(experiment, experiment_date):
-    """ check if new files are added """
+    """ check if experiment and experiment_date are missing in cloud """
     block_blob_service = get_cloud_content()
     blobContent = block_blob_service.list_blobs(azure_blob_container)
     blobFiles = []
@@ -59,18 +63,18 @@ def check_cloud_content(experiment, experiment_date):
     for f in blobFiles:
         if (experiment in f) and (experiment_date in f):
             missing = False
-            print("Experiment Date: " + experiment_date + " already in cloud.")
+            print("Experiment: " +experiment+", Date: " + experiment_date + " already in cloud.")
             break
     return missing        
             
 def cronos2cloud():
-    ftp = cronos_connect()
+    ftp = cronos_connect(True)
     folders_cronos = ftp.nlst(cronos_dir)
     folders_cronos = [name for name in folders_cronos if 'EMB' in name]
     ftp.quit()
     for experiment in folders_cronos:
         pi_mkdir(experiment)
-        ftp = cronos_connect()
+        ftp = cronos_connect(False)
         ftp.cwd(cronos_dir + '/' + experiment)
         experiment_dates = ftp.nlst()[:-1]
         ftp.quit()
@@ -79,7 +83,7 @@ def cronos2cloud():
             date = date_raw2[:-4].replace(" ","_")
             if check_cloud_content(experiment, date) == True:
                 pi_mkdir(experiment + '/' + date)
-                ftp = cronos_connect()
+                ftp = cronos_connect(False)
                 ftp.cwd(cronos_dir + '/' + experiment + '/' + date_raw2)
                 experiment_files = ftp.nlst()
                 for file_raw in experiment_files:
@@ -90,14 +94,27 @@ def cronos2cloud():
                         ftp.retrbinary("RETR " + file, lf.write)
                         lf.close()
                         send2cloud(raspberry_dir + '/' + experiment + '/' + date + '/' + file, azure_path + '/' + experiment + '/' + date)
-                ftp.rmd(date_raw2)
                 print("Experiment: "+experiment+", Date: "+date+", Cloud update done.")       
                 ftp.quit()
+            else:
+                ftp = cronos_connect(False)
+                ftp.cwd(cronos_dir + '/' + experiment + '/' + date_raw2)
+                experiment_files = ftp.nlst()
+                for file_raw in experiment_files:
+                    file = file_raw.split(None, 8)[-1]
+                    ftp.delete(file)
+                ftp.quit()
+                ftp = cronos_connect(False)
+                ftp.cwd(cronos_dir + '/' + experiment)                
+                ftp.rmd(date_raw2)
+                print("Experiment: "+experiment+", Date: "+date+", deleted from Cronos.")       
+                ftp.quit()
+                
 
         
 if __name__ == '__main__':
     while(1):
-        print("Cronos query starting...")
+        print("Cronos query running...")
         cronos2cloud()
-        print("Cronos query done.")
+        print("Cronos query done. Waiting one hour to start new query.")
         time.sleep(3600)
